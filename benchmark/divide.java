944158dbd234d8533d3351988313a41016115627
java2logic <prajkhowa@cse.ust.hk>
2016-07-11 22:46:57 +0800

    Add files via upload

---
+++ benchmark/divide.java
@@ -0,0 +1,16 @@
+
+public class divide{
+
+	
+	public void divide(int X,int Y) {
+	int r,q;
+    	r = X;
+    	q = 0;
+    	while(r >= Y) {
+      	r = r - Y;
+      	q = q + 1;
+    	}
+	
+	
+	}
+}
