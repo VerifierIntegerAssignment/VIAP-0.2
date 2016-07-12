6b8d3b130b436ad5f25cb9d3060327c8e7feeef3
java2logic <prajkhowa@cse.ust.hk>
2016-07-12 12:58:10 +0800

    Add files via upload

---
+++ benchmark/series8.java
@@ -0,0 +1,21 @@
+
+public class series8{
+	
+	
+	/**
+	 * @param args
+	 */
+	public void series8(int X) {
+		
+		int sum,i,X;
+		sum=0;
+		i=0;
+		while(i<X)
+		{
+			i=i+1;
+			sum=sum+i*i*i*i*i*i*i*i;
+			
+		}
+	}
+
+}
---
+++ benchmark/sumodd.java
@@ -0,0 +1,23 @@
+
+public class NSeries7 {
+
+
+	/**
+	 * @param args
+	 */
+	public static void NSeries7(int X) {
+		// TODO Auto-generated method stub
+		int sum,i;
+		sum=0;
+		i=0;
+		while(i<X)
+		{
+
+			sum=sum+2*i+1;
+			i=i+1;
+
+		}
+	}
+
+
+}
