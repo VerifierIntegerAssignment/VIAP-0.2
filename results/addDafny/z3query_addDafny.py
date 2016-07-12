98fa519078171cadf408bb0fbe62cd4e2e890837
java2logic <prajkhowa@cse.ust.hk>
2016-07-12 09:50:12 +0800

    VSTTE 2008

---
+++ results/addDafny/z3query_addDafny.py
@@ -0,0 +1,51 @@
+from z3 import *
+set_param(proof=True)
+_p1=Int('_p1')
+_p2=Int('_p2')
+Y=Int('Y')
+X=Int('X')
+r=Int('r')
+l=Int('l')
+Y1=Int('Y1')
+X1=Int('X1')
+r1=Int('r1')
+l1=Int('l1')
+_N1=Const('_N1',IntSort())
+_N2=Const('_N2',IntSort())
+_n2=Int('_n2')
+_n1=Int('_n1')
+power=Function('power',IntSort(),IntSort(),IntSort())
+_s=Solver()
+_s.add(ForAll([_p1],Implies(_p1>=0, power(0,_p1)==0)))
+_s.add(ForAll([_p1,_p2],Implies(power(_p2,_p1)==0,_p2==0)))
+_s.add(ForAll([_p1],Implies(_p1>0, power(_p1,0)==1)))
+_s.add(ForAll([_p1,_p2],Implies(power(_p1,_p2)==1,Or(_p1==1,_p2==0))))
+_s.add(ForAll([_p1,_p2],Implies(And(_p1>0,_p2>=0), power(_p1,_p2+1)==power(_p1,_p2)*_p1)))
+_s.set("timeout",60000)
+_s.add(Y1 == Y)
+_s.add(X1 == X)
+_s.add(r1 == If(Y < 0,X - _N1,X + _N2))
+_s.add(l1 == If(Y < 0,Y + _N1,Y - _N2))
+_s.add((Y + _N1==0))
+_s.add(Or(_N1==0,(Y + (_N1-1)!=0)))
+_s.add((Y - _N2==0))
+_s.add(Or(_N2==0,(Y - (_N2-1)!=0)))
+_s.add(_N1>=0)
+_s.add(_N2>=0)
+_s.add(Not(r1==X + Y))
+if sat==_s.check():
+	print "Counter Example"
+	print _s.model()
+elif unsat==_s.check():
+	_s.check()
+	if os.path.isfile('j2llogs.logs'):
+		file = open('j2llogs.logs', 'a')
+		file.write("\n**************\nProof Details\n**************\n"+str(_s.proof().children())+"\n")
+		file.close()
+	else:
+		file = open('j2llogs.logs', 'w')
+		file.write("\n**************\nProof Details\n**************\n"+str(_s.proof().children())+"\n")
+		file.close()
+	print "Successfully Proved"
+else:
+	print "Failed To Prove"
