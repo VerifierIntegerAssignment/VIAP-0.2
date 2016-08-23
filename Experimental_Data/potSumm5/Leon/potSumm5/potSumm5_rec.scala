579bbed4ba7d2c58a488736aabc6284f1178b833
VerifierIntegerAssignment <prajkhowa@ust.hk>
2016-08-23 15:05:09 +0800

    Delete potSumm5_rec.scala

--- Experimental_Data/potSumm5/Leon/potSumm5/potSumm5_rec.scala
+++
@@ -1,9 +0,0 @@
-object potSumm5 {
-        
-     def potSumm5(X: Int): Int = ({ 
-        require(X >= 0)
-        if (X == 0) 0
-        else    potSumm5(X-1)+X*X*X*X*X
-    })ensuring(_ == (2*X*X*X*X*X*X+6*X*X*X*X*X+5*X*X*X*X-X*X)/12)
-     
-     }
