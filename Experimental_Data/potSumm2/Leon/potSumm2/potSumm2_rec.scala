a7c79b0981c25cc1bf4d5b60968cc82aef460740
VerifierIntegerAssignment <prajkhowa@ust.hk>
2016-08-23 14:59:39 +0800

    Delete potSumm2_rec.scala

--- Experimental_Data/potSumm2/Leon/potSumm2/potSumm2_rec.scala
+++
@@ -1,9 +0,0 @@
-object potSumm2 {
-        
-     def potSumm2(X: Int): Int = ({ 
-        require(X >= 0)
-        if (X == 0) 0
-        else    potSumm2(X-1)+X*X
-    })ensuring(_ == X*(X+1)*(2*X+1)/6)
-     
-     }
