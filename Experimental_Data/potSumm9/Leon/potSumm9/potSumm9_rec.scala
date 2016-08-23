a261c49d4b9a53bdf9f648a950df7660bcf883f1
VerifierIntegerAssignment <prajkhowa@ust.hk>
2016-08-23 15:09:17 +0800

    Delete potSumm9_rec.scala

--- Experimental_Data/potSumm9/Leon/potSumm9/potSumm9_rec.scala
+++
@@ -1,9 +0,0 @@
-object potSumm9 {
-        
-     def potSumm9(X: Int): Int = ({ 
-        require(X >= 0 && X<=20)
-        if (X == 0) 0
-        else    potSumm9(X-1)+X*X*X*X*X*X*X*X*X
-    })ensuring(_ == (2*X*X*X*X*X*X*X*X*X*X+10*X*X*X*X*X*X*X*X*X+15*X*X*X*X*X*X*X*X-14*X*X*X*X*X*X+10*X*X*X*X-3*X*X)/20)
-     
-     }
