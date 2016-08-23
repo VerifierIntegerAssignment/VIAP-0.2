39cff2e841c56057a116f5a668de65b9cbbc7995
VerifierIntegerAssignment <prajkhowa@ust.hk>
2016-08-23 15:01:10 +0800

    Delete potSumm3_rec.scala

--- Experimental_Data/potSumm3/Leon/potSumm3/potSumm3_rec.scala
+++
@@ -1,9 +0,0 @@
-object potSumm3 {
-        
-     def potSumm3(X: Int): Int = ({ 
-        require(X >= 0)
-        if (X == 0) 0
-        else    potSumm3(X-1)+X*X*X
-    })ensuring(_ == X*(X+1)*X*(X+1)/4)
-     
-     }
