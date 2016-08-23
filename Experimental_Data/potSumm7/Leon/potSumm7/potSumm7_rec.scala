6b499395ccfd6aad359b494c77d2551dd936f6ef
VerifierIntegerAssignment <prajkhowa@ust.hk>
2016-08-23 15:07:23 +0800

    Delete potSumm7_rec.scala

--- Experimental_Data/potSumm7/Leon/potSumm7/potSumm7_rec.scala
+++
@@ -1,9 +0,0 @@
-object potSumm7 {
-        
-     def potSumm7(X: Int): Int = ({ 
-        require(X >= 0 && X<=20)
-        if (X == 0) 0
-        else    potSumm7(X-1)+X*X*X*X*X*X*X
-    })ensuring(_ == (3*X*X*X*X*X*X*X*X+12*X*X*X*X*X*X*X+14*X*X*X*X*X*X-7*X*X*X*X+2*X*X)/24)
-     
-     }
