d3402b8e3d64be56db66b412ad6bc8b62779be7b
VerifierIntegerAssignment <prajkhowa@ust.hk>
2016-08-24 18:10:13 +0800

    power

--- Experimental_Data/power/Leon/power/power.scala
+++ Experimental_Data/power/Leon/power/power.scala
@@ -17,7 +17,7 @@ object power {
 
      def power(a: Int,b: Int): Int = ({ 
         require(a >= 0 && b >= 0)
-        if (a == 0) 0 else if (b == 0) 1 else power(a,b-1)*a
+        if (a == 0 && b>0) 0 else if (b == 0) 1 else power(a,b-1)*a
     })
 
 }
