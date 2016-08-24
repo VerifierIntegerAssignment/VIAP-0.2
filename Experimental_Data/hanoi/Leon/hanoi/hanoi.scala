e12a31da0f45adfab472576bb96eef39d410e3b0
VerifierIntegerAssignment <prajkhowa@ust.hk>
2016-08-24 18:08:24 +0800

    hanoi

--- Experimental_Data/hanoi/Leon/hanoi/hanoi.scala
+++ Experimental_Data/hanoi/Leon/hanoi/hanoi.scala
@@ -15,9 +15,10 @@ object hanoi {
     })ensuring(_ == power(2,x)-1) 
 
 
-     def power(a: Int,b: Int): Int = { 
+def power(a: Int,b: Int): Int = { 
         require(a >= 0 && b >= 0)
-        if (b == 0) 1
+        if (a == 0 && b>0) 0
+        else if (b == 0) 1
         else    power(a,b-1)*a
     }
 
